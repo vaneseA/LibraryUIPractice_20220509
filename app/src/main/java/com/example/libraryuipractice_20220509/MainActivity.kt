b69aa6e13cb51ecaaa8e2c48784d1f84e6c7f698
DK <spol@naver.com>
2022-05-09 15:11:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/libraryuipractice_20220509/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.libraryuipractice_20220509
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setupValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setupValues(){
+
+    }
+}
