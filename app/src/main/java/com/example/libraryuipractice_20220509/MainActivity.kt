fb07f89f9b7bd5588e9a224b160514e560718e39
DK <spol@naver.com>
2022-05-09 17:01:37 +0900

    라이브러리 사용

--- app/src/main/java/com/example/libraryuipractice_20220509/MainActivity.kt
+++ app/src/main/java/com/example/libraryuipractice_20220509/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.libraryuipractice_20220509
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents(){
-
+        profileImg.setOnClickListener {
+            val myIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 
     fun setupValues(){
--- app/src/main/java/com/example/libraryuipractice_20220509/ProfileActivity.kt
+++ app/src/main/java/com/example/libraryuipractice_20220509/ProfileActivity.kt
@@ -1,4 +1,12 @@
 package com.example.libraryuipractice_20220509
 
-class ProfileActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
 }
